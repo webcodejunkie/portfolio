154d84f0d9f0ce683975093e67994afe50355dd1
Rafael J <66970141+webcodejunkie@users.noreply.github.com>
2022-09-27 13:06:19 -0400

    added hashrouter

--- src/index.js
+++ src/index.js
@@ -4,13 +4,16 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Modal from 'react-modal';
+import { HashRouter } from 'react-router-dom';
 
 Modal.setAppElement('#root');
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <HashRouter>
+      <App />
+    </HashRouter>
   </React.StrictMode>
 );
 
