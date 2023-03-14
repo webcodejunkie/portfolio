a1c19ee4d2da1d9746957c0bae29bccf6eb5a9b8
Rafael J <66970141+webcodejunkie@users.noreply.github.com>
2023-03-13 21:10:01 -0400

    added new belt cover photos and update component

--- src/components/belt/belt-component.js
+++ src/components/belt/belt-component.js
@@ -3,9 +3,9 @@ import '../../output/css/belt.css';
 function Belt() {
 	return (
 		<div className='belt-container'>
-			<div></div>
-			<div></div>
-			<div></div>
+			<div className='beltElOne'></div>
+			<div className='beltElTwo'></div>
+			<div className='beltElThree'></div>
 		</div>
 	)
 }
