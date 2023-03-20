4f66a434cbad025ad6e406c20f7f19ad17a51c4f
Rafael J <66970141+webcodejunkie@users.noreply.github.com>
2023-03-20 09:54:00 -0400

    introduce react-modal component to belt

--- src/components/belt/belt-component.js
+++ src/components/belt/belt-component.js
@@ -1,11 +1,39 @@
 import '../../output/css/belt.css';
+import { useState } from 'react';
+import Modal from 'react-modal';
+import { faCircleXmark } from '@fortawesome/free-regular-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 function Belt() {
+	const [modalIsOpen, setIsOpen] = useState(false);
+
+	function openModal() {
+		setIsOpen(true);
+	}
+
+	function closeModal() {
+		setIsOpen(false);
+	}
+
 	return (
 		<div className='belt-container'>
-			<div className='beltElOne'></div>
-			<div className='beltElTwo'></div>
-			<div className='beltElThree'></div>
+			<div className='beltElOne' onClick={openModal}>
+				<Modal
+					isOpen={modalIsOpen}
+				>
+					<h1>Skullify</h1>
+					<FontAwesomeIcon
+						icon={faCircleXmark}
+						onClick={() => { setIsOpen(false) }}
+					/>
+				</Modal>
+			</div>
+			<div className='beltElTwo'>
+
+			</div>
+			<div className='beltElThree'>
+
+			</div>
 		</div>
 	)
 }
